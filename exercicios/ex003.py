7c29f9f52df6274afea5b0d8c74cf5f5d03717f3
ingrinite <ingridalcantara@ufam.edu.br>
2025-03-13 05:41:22 -0300

    alterado o ex003

--- exercicios/ex003.py
+++ exercicios/ex003.py
@@ -2,3 +2,9 @@ n1 = int(input('Digite o primeiro número:'))
 n2 = int(input('Digite o segundo numero:'))
 soma = n1+n2
 print('A soma de {} e {} é igual a {}' .format(n1, n2, soma))
+
+nome = input('Qual seu nome? ')
+dia = input('Em que dia você nasceu? ')
+mes = input('Em qual mês você nasceu? ')
+ano = input('Em que ano você nasceu? ')
+print('Olá, {}. Você nasceu no dia {} de {} de {}, certo?' .format(nome, dia, mes, ano))
