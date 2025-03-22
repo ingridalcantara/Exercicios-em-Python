026f2509ee244532d1c106fe73a926c390aaad8f
ingrinite <ingridalcantara@ufam.edu.br>
2025-03-22 06:56:57 -0300

    exercicios

---
+++ exercicios_mundo2/ex053.py
@@ -0,0 +1,2 @@
+# Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+# Ex: apos a sopa, a sacada da casa (frases que podem ser lidas de tras pra frente e vice-versa)
