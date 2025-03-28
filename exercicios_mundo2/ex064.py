01dd4fbce6d6b878e6772352de091c0b9e4643f4
ingrinite <ingridalcantara@ufam.edu.br>
2025-03-28 06:10:54 -0300

    fim da aula 14

--- exercicios_mundo2/ex064.py
+++ exercicios_mundo2/ex064.py
@@ -1,1 +1,15 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
+'''n = soma = cont = 0
+while n != 999:
+    n = int(input('Digite um número: '))
+    soma += n
+    cont += 1
+print(f'Você digitou {cont-1} números e a soma deles é {soma-999}')'''
+
+n = soma = cont = 0
+n = int(input('Digite um número: '))
+while n != 999:
+    soma += n
+    cont += 1
+    n = int(input('Digite um número: '))
+print(f'Você digitou {cont} números e a soma deles é {soma}')
--- exercicios_mundo2/ex065.py
+++ exercicios_mundo2/ex065.py
@@ -1,1 +1,18 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+n = 'S'
+media = quant = soma = maior = menor = 0
+while n == 'S':
+    num = int(input('Digite um número: '))
+    n = str(input('Quer continuar? [S/N] ')).strip()[0].upper()
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            num = maior
+        if num < menor:
+            menor = num
+media = soma / quant
+print(f'Você digitou {quant} e a média entre eles é {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
