eeea2befd05d01151c6db568421489475275aa66
ingrinite <ingridalcantara@ufam.edu.br>
2025-03-29 06:38:04 -0300

    ultimos exercicios aula 15

--- exercicios_mundo2/ex070.py
+++ exercicios_mundo2/ex070.py
@@ -1,3 +1,6 @@
 '''
-Crie um programa que leia o nome
+Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. No final mostre:
+    - Qual é o total gasto na compra.
+    - Quantos produtos custam mais de R$1000.
+    - Qual é o nome do produto mais barato.
 '''
