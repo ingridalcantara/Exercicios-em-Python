22ca936fb3839c123decee031b8c1e53a57fcfc4
ingrinite <ingridalcantara@ufam.edu.br>
2025-04-02 12:27:19 -0400

    exercicios aula 16

--- exercicios_mundo3/ex077.py
+++ exercicios_mundo3/ex077.py
@@ -1,1 +1,7 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+palavras = ('jogar', 'trabalhar', 'dormir', 'gratis', 'aviao', 'ventilador', 'ar condicionado')
+for item in palavras:
+    print(f'\nNa palavra {item.upper()} temos ', end='')
+    for letra in item:
+        if letra.lower() in 'aeiou':
+            print(letra, end='')
