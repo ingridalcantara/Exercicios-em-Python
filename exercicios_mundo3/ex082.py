3962e6371988564c70bec549f6cb6e67137f65fc
ingrinite <ingridalcantara@ufam.edu.br>
2025-04-04 11:20:44 -0400

    ex083

--- exercicios_mundo3/ex082.py
+++ exercicios_mundo3/ex082.py
@@ -4,6 +4,8 @@ Depois disso, crie duas listas extras que vão conter apenas os valores pares e 
 Ao final, mostre o conteúdo das três listas geradas.
 '''
 numeros = []
+par = list()
+impar = list()
 while True:
     numeros.append(int(input('Digite um valor: ')))
     pergunta = ' '
@@ -11,3 +13,11 @@ while True:
         pergunta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if pergunta == 'N':
         break
+for i, v in enumerate(numeros):
+    if v % 2 == 0:
+        par.append(v)
+    else:
+        impar.append(v)
+print(f'Lista original: {numeros}')
+print(f'Lista par: {par}')
+print(f'Lista ímpar: {impar}')
--- exercicios_mundo3/ex083.py
+++ exercicios_mundo3/ex083.py
@@ -1,1 +1,3 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
+exp = str(input('Digite alguma coisa: '))
+print(exp)
