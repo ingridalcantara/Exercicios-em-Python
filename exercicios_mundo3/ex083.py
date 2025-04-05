3d1b97c1d31ba05d1dbe8427a2c6957146fb532f
ingrinite <ingridalcantara@ufam.edu.br>
2025-04-05 03:50:32 -0300

    finaliza ex aula 16

--- exercicios_mundo3/ex083.py
+++ exercicios_mundo3/ex083.py
@@ -1,3 +1,16 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
-exp = str(input('Digite alguma coisa: '))
-print(exp)
+exp = str(input('Digite uma expressão matemática: '))
+pilha = list()
+for simb in exp:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Expressão válida!')
+else:
+    print('Expressão inválida!')
